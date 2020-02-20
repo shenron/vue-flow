584ce6c3d7a224029cf50dfe7e755195a9229d03
Thomas CASTELLY <thomas@tcy.io>
2020-02-20 22:22:54 +0100

    remove "fake" error

--- src/main.js
+++ src/main.js
@@ -3,19 +3,11 @@
 import Vue from 'vue';
 import CompositionApi from '@vue/composition-api';
 import App from '@/App.vue';
-import type { DemoType } from './DemoType.js.flow'; // eslint-disable-line no-unused-vars
 
 Vue.config.productionTip = false;
 Vue.use(CompositionApi);
 
-// uncomment to see errors
 const hello: string = 'Welcome!';
-// const myDemoType: DemoType = {
-//   str: () => {},
-//   func: () => {},
-// };
-// hello = 10;
-// console.log(hello, myDemoType);
 
 new Vue({
   render: (h) => h(App),
